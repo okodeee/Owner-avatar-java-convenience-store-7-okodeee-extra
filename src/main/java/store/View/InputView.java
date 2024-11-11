96c248e6e1dbe16bc9542fe4aec1d25b6bad1825
okod <okodee@naver.com>
2024-11-11 17:03:05 +0900

    feat: 기타 잘못된 입력의 경우에 대한 예외 처리 구현

--- src/main/java/store/View/InputView.java
+++ src/main/java/store/View/InputView.java
@@ -31,26 +31,51 @@ public class InputView {
     }
 
     public boolean askPromotionAddition(String productName, int additionalQuantity) {
-        System.out.printf("\n현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", productName, additionalQuantity);
-        String response = Console.readLine().trim().toUpperCase();
-        return response.equals("Y");
+        while (true) {
+            System.out.printf("\n현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", productName, additionalQuantity);
+            String response = Console.readLine().trim().toUpperCase();
+            if (validateResponse(response)) {
+                return response.equals("Y");
+            }
+        }
     }
 
     public boolean askPartiallyRegularPrice(String productName, int quantity) {
-        System.out.printf("\n현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", productName, quantity);
-        String response = Console.readLine().trim().toUpperCase();
-        return response.equals("Y");
+        while (true) {
+            System.out.printf("\n현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", productName, quantity);
+            String response = Console.readLine().trim().toUpperCase();
+            if (validateResponse(response)) {
+                return response.equals("Y");
+            }
+        }
     }
 
     public boolean askMembershipDiscount() {
-        System.out.print("\n멤버십 할인을 받으시겠습니까? (Y/N)\n");
-        String response = Console.readLine().trim().toUpperCase();
-        return response.equals("Y");
+        while (true) {
+            System.out.print("\n멤버십 할인을 받으시겠습니까? (Y/N)\n");
+            String response = Console.readLine().trim().toUpperCase();
+            if (validateResponse(response)) {
+                return response.equals("Y");
+            }
+        }
     }
 
     public boolean askAdditionalOrder() {
-        System.out.print("\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n");
-        String response = Console.readLine().trim().toUpperCase();
-        return response.equals("Y");
+        while (true) {
+            System.out.print("\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n");
+            String response = Console.readLine().trim().toUpperCase();
+            if (validateResponse(response)) {
+                return response.equals("Y");
+            }
+        }
+    }
+
+    private static boolean validateResponse(String response) {
+        if (response.equals("Y") || response.equals("N")) {
+            return true;
+        } else {
+            System.out.println("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+        }
+        return false;
     }
 }
